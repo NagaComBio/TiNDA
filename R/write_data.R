d3ce8e1552e89c30057206dd404bcfb607b333e8
naga.rna@gmail.com <naga.rna@gmail.com>
2019-04-05 12:09:44 +0200

    New write_data function

---
+++ R/write_data.R
@@ -0,0 +1,10 @@
+#' Write TiNDA data
+#' 
+#' Function to write TiNDA data to a tsv file with TiN_Class information
+#' 
+#' @param tinda_object TiNDA object produced by the main TiNDA function
+#' @param file_name Name of the file
+#' 
+write_data <- function(tinda_object, file_name){
+  write_tsv(tinda_object$data, file_name)
+}
